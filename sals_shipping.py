8c0cb40e8243f8c22147b4195dfdca2a9d0138b1
joshuahenrykite <you@example.com>
2020-03-23 15:57:30 +1100

    Added calculation functionality

--- sals_shipping.py
+++ sals_shipping.py
@@ -21,3 +21,21 @@ def drone_shipping(weight):
   elif weight > 10.0:
     cost = (weight * 14.25)
   return cost
+
+def print_best_shipping_method(weight):
+  ground = ground_shipping(weight)
+  premium = premium_ground_shipping
+  drone = drone_shipping(weight)
+  
+  if ground < premium and ground < drone:
+    method = "standard ground"
+    cost = ground
+  
+  elif premium < ground and premium < drone:
+    method =  "premium ground"
+    cost = premium
+  
+  elif drone < ground and drone < premium:
+    method = "drone"
+    cost = drone
+
